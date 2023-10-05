91da70d99fce04e622a677114219c320b6bfd9f0
Xylorific <396958@colum.edu>
2023-10-05 13:53:18 -0500

    Update: made data flow like a stream

---
+++ CSV_Pipe_To_TabDelimited/CSV_Pipe_To_TabDelimited/Exporter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CSV_Pipe_To_TabDelimited
+{
+    //a class for exporter related methods
+    internal class Exporter
+    {
+
+        /// <summary>
+        ///A method used for exporting a list of strings as a txt file to a desired directory
+        /// </summary>
+        /// <param name="parsedData"></param>The parsed data from a delimited file
+        /// <param name="directory"></param>The desired output directory for the txt file
+        public static void ExportStream(int lineCounter, string data, StreamWriter writer)
+        {
+            
+            try
+            {
+                writer.WriteLine("Line#"+lineCounter+" :" + data);
+                writer.WriteLine();
+                                        
+            }
+            catch(Exception exportError)
+            {
+                ErrorLog.LogError(exportError.ToString(), data);
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+
+
+        /// <summary>
+        /// A method used to export the error log to a txt file
+        /// </summary>
+        /// <param name="directory"></param>The desired output directory
+        public static void ExportErrorLog(string directory)
+        {
+            try
+            {
+                using (FileStream fileStream = new FileStream(Path.Combine(directory, "ErrorLog.txt"), FileMode.Create))
+                {
+                    using (StreamWriter writer = new StreamWriter(fileStream))
+                    {
+                        errors.Clear();
+                        errors = ErrorLog.ReturnErrorLog();
+
+                        foreach(string error in errors)
+                        {
+                            writer.WriteLine(error);
+                            writer.WriteLine();
+                        }
+                        writer.Close();
+                    }
+                    fileStream.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorLog.LogError(e.ToString(), scriptName);
+            }
+        }
+        */
+
+    }
+}
---
+++ CSV_Pipe_To_TabDelimited/CSV_Pipe_To_TabDelimited/Importer.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV_Pipe_To_TabDelimited
+{
+    internal class Importer
+    {
+
+        public static string? fileExtension;
+
+        public static char delimiter;
+
+        static string? currentLineInFile;
+
+        static int lineCounter;
+
+        static string? fileName;
+
+        /// <summary>
+        /// Import all data from desired file. This is currently geared toward txt
+        /// files.
+        /// </summary>
+        /// <param name="path"></param>This is the relative path of a file
+        /// <returns>A list of all data from the file</returns>
+        public static void StreamData(List<string> inputPaths, string outputPath)
+        {
+            ClearOutputDirectory(outputPath);
+
+            foreach (string currentFilePath in inputPaths)
+            {
+                //checks if the file extension is valid
+                FilterFileByExtension(currentFilePath);
+
+                if(fileExtension != null)
+                { 
+                    fileName = Path.GetFileName(currentFilePath);
+
+                    delimiter = MainWindow.extensions_And_Delimiter_To_Parse[fileExtension];
+
+
+                    using (StreamReader reader = new StreamReader(currentFilePath))
+                    {
+                        using (FileStream fileStream = new FileStream(Path.Combine(outputPath,fileName), FileMode.OpenOrCreate))
+                        {
+                            using(StreamWriter writer = new StreamWriter(fileStream))
+                            { 
+                                lineCounter = 1;
+
+                                //Read the script until null
+                                while ((currentLineInFile = reader.ReadLine()) != null)
+                                {
+                                    
+                                    //parse the line of data
+                                    ParseDelimitedFile.ParseDelimterLine(currentLineInFile, delimiter);
+
+                                    //Export that line of data
+                                    Exporter.ExportStream(lineCounter, ParseDelimitedFile.parsedData, writer);
+
+                                    lineCounter++;
+                                }
+                                writer.Close();
+                            }
+                            fileStream.Close();
+                        }
+                        reader.Close();
+                    }
+                }
+
+            }   
+
+        } 
+
+
+
+        static void FilterFileByExtension(string path)
+        {
+            if (MainWindow.validFileExtensions.Any(ext => path.EndsWith(fileExtension = ext)))
+            {
+
+            }
+            else
+            {
+                fileExtension = null;
+                ErrorLog.LogError("Invalid extension", path);
+            }   
+        }
+
+
+
+
+        static void ClearOutputDirectory(string outputPath)
+        {
+            var dir = Directory.GetFiles(outputPath);
+            foreach(string file in dir)
+            {
+                File.Delete(file);
+            }
+        }
+
+
+    }
+
+}
+
+
+    
+
